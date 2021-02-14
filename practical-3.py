36b98062e7e19de675d376582f7f109ef9efaef1
domswaine <dominic.swaine@gmail.com>
2021-02-14 02:15:43 +0000

    Implement practical 3, section 5

--- practical-3.py
+++ practical-3.py
@@ -1,7 +1,9 @@
 from os import system
+from matplotlib import pyplot
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn import tree, metrics
+from sklearn import tree, metrics, linear_model, preprocessing
+from numpy import arange, meshgrid, array
 
 print("Section 2", "\n")
 
@@ -19,7 +21,7 @@ print("\n")
 print("Section 3", "\n")
 
 def get_counts(c):
-    return "setosa: %s, versicolor: %s, virginica: %s" % (c[0], c[1], c[2])
+    return "setosa: %s, versicolor: %s, virginica: %s" % (round(c[0], 2), round(c[1], 2), round(c[2], 2))
 
 def count_on_attribute(X, y, no_classes, features_extractor):
     class_counts = {}
@@ -79,10 +81,10 @@ print("1R classifier accuracy: %s" % round(get_accuracy(iris.target, y_hat, M), 
 
 del M, sepal_lengths, sepal_length, class_counts, sepal_width, sepal_widths
 del slw_extractor, sepal_lengths_widths, length, width, clf, y_hat
-print("")
+print("\n")
 
 
-print("Section 4")
+print("Section 4", "\n")
 
 X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=0)
 clf = tree.DecisionTreeClassifier(random_state=0)
@@ -96,28 +98,36 @@ accuracy = (count / M_test) * 100
 print("Number of correct predictions: %d out of %d = %f%%" % (count, M_test, accuracy))
 del count, M_test, accuracy
 
-training_score = clf.score(X_train, y_train) * 100
-test_score = clf.score(X_test, y_test) * 100
+def get_training_score():
+    training_score = clf.score(X_train, y_train) * 100
+    print("Training score: %d%%" % round(training_score, 2))
 
-print("Training score: %f%%" % training_score)
-print("Test score: %f%%" % test_score)
-del training_score, test_score
+def get_test_score():
+    test_score = clf.score(X_test, y_test) * 100
+    print("Test score: %d%%" % round(test_score, 2))
 
-sklearn_accuracy = metrics.accuracy_score(y_test, y_hat)
-print("Accuracy (sk-learn): %f%%" % sklearn_accuracy)
-del sklearn_accuracy
+def get_accuracy():
+    sklearn_accuracy = metrics.accuracy_score(y_test, y_hat)
+    print("Accuracy (sk-learn): %f%%" % round(sklearn_accuracy, 2))
 
-precision = metrics.precision_score(y_test, y_hat, average=None)
-print("Precision: %s" % get_counts(precision))
-del precision
+def get_precision():
+    precision = metrics.precision_score(y_test, y_hat, average=None)
+    print("Precision: %s" % get_counts(precision))
 
-recall = metrics.recall_score(y_test, y_hat, average=None)
-print("Recall: %s" % get_counts(recall))
-del recall
+def get_recall():
+    recall = metrics.recall_score(y_test, y_hat, average=None)
+    print("Recall: %s" % get_counts(recall))
 
-f1 = metrics.f1_score(y_test, y_hat, average=None)
-print("F1: %s" % get_counts(f1))
-del f1
+def get_f1():
+    f1 = metrics.f1_score(y_test, y_hat, average=None)
+    print("F1: %s" % get_counts(f1))
+
+get_training_score()
+get_test_score()
+get_accuracy()
+get_precision()
+get_recall()
+get_f1()
 
 # print(clf.decision_path(iris.data))
 # tree.export_graphviz(clf, out_file="plots/p3-iris-decision-tree.dot", class_names=classes, impurity=True)
@@ -125,3 +135,64 @@ del f1
 # system("dot plots/p3-iris-decision-tree.dot -Tpng > plots/p3-iris-decision-tree.png")
 
 del X_test, X_train, clf, y_hat, y_test, y_train
+print("\n")
+
+
+print("Section 5", "\n")
+
+X, y = iris.data[:, :2], iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+clf = linear_model.LogisticRegression(solver="lbfgs", multi_class="multinomial")
+clf.fit(X_train, y_train)
+y_hat = clf.predict(X_test)
+
+get_training_score()
+get_test_score()
+get_accuracy()
+get_precision()
+get_recall()
+get_f1()
+
+pyplot.figure()
+pyplot.xticks(fontsize=14)
+pyplot.yticks(fontsize=14)
+pyplot.set_cmap("Blues")
+pyplot.xlabel(attributes[0], fontsize=14)
+pyplot.ylabel(attributes[1], fontsize=14)
+
+interval = 0.1
+x0_range = arange(min(X[:,0]) - interval, max(X[:,0]) + interval, interval)
+x1_range = arange(min(X[:,1]) - interval, max(X[:,1]) + interval, interval)
+X_pairs = array([[x0, x1] for x1 in x1_range for x0 in x0_range])
+y_hat_pairs = clf.predict(X_pairs)
+x0_mesh, x1_mesh = meshgrid(x0_range, x1_range)
+y_hat_mesh = y_hat_pairs.reshape(x0_mesh.shape)
+pyplot.pcolormesh(x0_mesh, x1_mesh, y_hat_mesh, shading="auto")
+
+markers = ['o','<','s']
+colours = [(0.957, 0.263, 0.212, 1), (0.545, 0.765, 0.029, 0.7), (0.118, 0.533, 0.898, 0.5)]
+
+for Xi, yi in zip(X, y):
+    pyplot.plot(Xi[0], Xi[1], marker=markers[yi], markerfacecolor=colours[yi], markersize=9, markeredgecolor='w')
+
+pyplot.savefig("plots/p3-q4-logistic-regression-decision-bounaries")
+
+pyplot.figure()
+pyplot.xticks(fontsize=14)
+pyplot.yticks(fontsize=14)
+pyplot.xlabel("False postive rate", fontsize=14)
+pyplot.ylabel("True postive rate", fontsize=14)
+
+conf_scores = clf.decision_function(X_pairs)
+y_binary = preprocessing.label_binarize(y_hat_pairs, classes=sorted(set(y)))
+for c in range(3):
+    fpr, tpr, _ = metrics.roc_curve(y_binary[:, c], conf_scores[:, c])
+    pyplot.plot(fpr, tpr, label=classes[c], color=colours[c])
+
+pyplot.legend()
+pyplot.savefig("plots/p3-q4-logistic-regression-roc-curve")
+
+del X, y, X_train, X_test, y_train, y_test, clf, y_hat
+del interval, x0_range, x1_range, X_pairs, y_hat_pairs, x0_mesh, x1_mesh, y_hat_mesh
+del markers, colours, Xi, yi, conf_scores, y_binary, c, fpr, tpr
+print("\n")
